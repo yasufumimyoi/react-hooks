97e27dac8b0af1007f701f4464563951ced72c55
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-09-26 21:16:11 +0900

    adding hooks

--- src/index.js
+++ src/index.js
@@ -1,95 +1,23 @@
-import React, { Component } from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
-import Counter from "./counter";
+import { countReducer, initialState } from "./reducer";
 
-class IndecisionApp extends Component {
-  render() {
-    const title = "Indecision App";
-    const options = ["Eat dinner", "Clean the room", "Watch YouTube"];
+const CountApp = () => {
+  const [state, dispatch] = useReducer(countReducer, initialState);
 
-    return (
-      <div>
-        <Header title={title} />
-        <Action />
-        <Options options={options} />
-        <AddOption />
-      </div>
-    );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <p>Put your lif in the hands of a computer</p>
-      </div>
-    );
-  }
-}
-
-class Action extends Component {
-  handlePick() {
-    alert("Hello");
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.handlePick}>What Should I do?</button>
-      </div>
-    );
-  }
-}
-
-class Options extends Component {
-  handleRemoveAll() {
-    alert("Hello");
-  }
-  render() {
-    return (
-      <div>
-        {this.props.options.map((option) => (
-          <Option key={option} optionText={option} />
-        ))}
-      </div>
-    );
-  }
-}
-
-class Option extends Component {
-  render() {
-    return <div>{this.props.optionText}</div>;
-  }
-}
-
-class AddOption extends Component {
-  handleOption(e) {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-
-    if (option) {
-      alert(option);
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <form>
-          <input type="text" name="option" />
-          <button>Add Option</button>
-        </form>
-        <Counter />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h3>Count App</h3>
+      <h4>Count: {state.count}</h4>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <IndecisionApp />
+    <CountApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,9 @@ export const initialState = { count: 0 };
 export const countReducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { count: state.count + 1 };
+      return { ...state, count: state.count + 1 };
     case "decrement":
-      return { count: state.count - 1 };
+      return { ...state, count: state.count - 1 };
     default:
       return state;
   }
