ce05d0f291f1934c40ce370fa4ab9ca7a80e9e18
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-10-15 21:07:00 +0900

    setting up more routing of individual videos

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,44 +3,42 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import HeaderRouter from "./HeaderRouter";
+import { useHistory } from "react-router-dom";
+import SchoolIcon from "@material-ui/icons/School";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     marginBottom: "40px",
   },
-  menuButton: {
+  logo: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
-  },
-  link: {
-    marginRight: "8px",
-    color: "white",
-    textDecoration: "none",
+    cursor: "pointer",
   },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const history = useHistory();
+
+  const handleRoute = () => {
+    history.push("/courses");
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
+          <SchoolIcon className={classes.logo} />
+          <Typography
+            variant="h6"
+            className={classes.title}
+            onClick={handleRoute}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
             React Study Videos
           </Typography>
           <HeaderRouter />
