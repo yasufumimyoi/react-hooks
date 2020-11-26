576b9bef70770b20d01d5095fb1bb995ad4503ac
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-11-26 23:03:47 +0900

    adding more videos

---
+++ src/videos/FirebaseVideo.js
@@ -0,0 +1,61 @@
+const MaterialUIVideos = [
+  {
+    id: 1,
+    url: "https://www.youtube.com/watch?v=meofoNuK3vo&t=133s",
+    image: "http://img.youtube.com/vi/meofoNuK3vo/mqdefault.jpg",
+    title: "Firebase Hosting をウェブで始めよう！ - Firecasts",
+    path: "/courses/firebase/1",
+    completed: false,
+  },
+  {
+    id: 2,
+    url: "https://www.youtube.com/watch?v=unr4s3jd9qA&t=329s",
+    image: "http://img.youtube.com/vi/unr4s3jd9qA/mqdefault.jpg",
+    title:
+      "Firebase React Authentication Tutorial For Beginners - Private Route With Hooks",
+    path: "/courses/firebase/2",
+    completed: false,
+  },
+  {
+    id: 3,
+    url: "https://www.youtube.com/watch?v=zQyrwxMPm88",
+    image: "http://img.youtube.com/vi/zQyrwxMPm88/mqdefault.jpg",
+    title: "I built a chat app in 7 minutes with React & Firebase",
+    path: "/courses/firebase/3",
+    completed: false,
+  },
+  {
+    id: 4,
+    url: "https://www.youtube.com/watch?v=mmmaeHBCTOw",
+    image: "http://img.youtube.com/vi/mmmaeHBCTOw/mqdefault.jpg",
+    title: "Firebase Hosting Tutorial #1 - Intro & Installation",
+    path: "/courses/firebase/4",
+    completed: false,
+  },
+  {
+    id: 5,
+    url: "https://www.youtube.com/watch?v=ApG8L2RKrSI&t=350s",
+    image: "http://img.youtube.com/vi/ApG8L2RKrSI/mqdefault.jpg",
+    title: "Web の Firebase 匿名認証を開始するには- Firecasts",
+    path: "/courses/firebase/5",
+    completed: false,
+  },
+  {
+    id: 6,
+    url: "https://www.youtube.com/watch?v=kX8by4eCyG4",
+    image: "http://img.youtube.com/vi/kX8by4eCyG4/mqdefault.jpg",
+    title: "Server-side Firebase Authentication Using Express JS",
+    path: "/courses/firebase/6",
+    completed: false,
+  },
+  {
+    id: 7,
+    url: "https://www.youtube.com/watch?v=8cOlDAxFmxM&t=114s",
+    image: "http://img.youtube.com/vi/8cOlDAxFmxM/mqdefault.jpg",
+    title: "Firebase Authentication State Persistence",
+    path: "/courses/firebase/7",
+    completed: false,
+  },
+];
+
+export default MaterialUIVideos;
